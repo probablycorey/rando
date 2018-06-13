950e9cb43c28ff397b7226874afccdd53b8b0185
Jon Maddox <jon@jonmaddox.com>
2018-06-13 14:08:57 -0400

    added some packages

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('./lib/hacks');
+
 const MUPPETS = ["Kermit", "Miss Piggy", "Fozzie", "Animal", "Scooter", "Skeeter", "Rowlf", "Gonzo", "Bunsen", "Beaker"]
 
 exports.randomMuppet = (req, res) => {
   const muppet = MUPPETS[Math.floor(Math.random()*MUPPETS.length)]
+  console.log(`Returned a muppet: ${muppet}`)
   res.send(muppet)
 }
