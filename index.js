215d6f471c96d3d885c98154e92af0df27e877f4
Jon Maddox <jon@jonmaddox.com>
2018-06-21 14:10:23 -0400

    :fire: noodle lib

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-require('noodle');
-
 const MUPPETS = ["Kermit", "Miss Piggy", "Fozzie", "Animal", "Scooter", "Skeeter", "Rowlf", "Gonzo", "Bunsen", "Beaker"]
 
 exports.randomMuppet = (req, res) => {
