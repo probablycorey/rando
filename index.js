29927809d33668b4766dcd28020aa0208c342a9d
Corey Johnson <probablycorey@github.com>
2018-07-02 14:52:03 -0700

    This will work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const moment = require('moment')
 
 exports.main = (req, res) => {
-  return moment().format("[THE DATE IS] MMM Do YY")
+  let output = moment().format("MMM Do YY")
+  res.send(output)
 }
