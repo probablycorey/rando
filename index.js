fdb82cf99466cef039d819916fa48c40e014a907
Corey Johnson <probablycorey@github.com>
2018-08-10 13:14:41 -0700

    cool shit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const handlebars = require('handlebars')
 const fetch = require('node-fetch')
 const fs = require('fs')
-
+//bloop
 let template = fs.readFileSync(__dirname + '/index.html').toString()
 
 exports.main = (req, res) => {
