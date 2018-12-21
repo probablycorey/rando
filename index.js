85779fb4cde39255a72bb0b23093a1704265946c
Corey Johnson <probablycorey@gmail.com>
2018-12-21 15:10:20 -0800

    poop

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// This is the code that runs!!!! ok!
+// Don't change this code
 const express = require('express')
 
 const port = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1><marquee direction=right>Hello from the outside!! At least I can say that I&#39;ve tried!</marquee></h1>')
+    res.send('<h1><marquee direction=right>💩</marquee></h1>')
     res.end()
 })
 
