b234b0fe2e448d541f9674877bb9cff6ac259b32
Corey Johnson <probablycorey@gmail.com>
2019-01-15 13:01:34 -0800

    ok

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-8998
+import {ApolloClient, InMemoryCache, HttpLink, ApolloLink} from 'apollo-boost'
+import {RestLink} from 'apollo-link-rest'
+import {createPersistedQueryLink} from 'apollo-link-persisted-queries'
+import fetch from 'isomorphic-unfetch'
+import getCsrfToken from '../util/getCsrfToken'
+import {onError} from 'apollo-link-error'
+import errorHandlerRegistry from './apolloErrorHandlerRegistry'
+
+const isBrowser = process.browser
+
+// Polyfill fetch() on the server (used by apollo-client)
+if (!isBrowser) {
+  ;(global as any).fetch = fetch
+  ;(global as any).Headers = require('node-fetch').Headers
+}
+
+let apolloClient: ApolloClient<{}> | null = null
+
+const persistedQueryLink = createPersistedQueryLink()
+
+function create(initialState: any, headers: {[key: string]: string} = {}) {
+  if (isBrowser) headers['CSRF-Token'] = getCsrfToken()
+
+  const hostUrl = isBrowser ? location.origin : process.env.HOST_URL
+  const httpLink = new HttpLink({
+    uri: `${hostUrl}/graphql`, // Server URL (must be absolute)
+    credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+    headers,
+  })
+  const restLink = new RestLink({
+    uri: hostUrl,
+    credentials: 'same-origin',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+  })
+  const errorLink = onError(e => errorHandlerRegistry.dispatch(e))
+  return new ApolloClient({
+    connectToDevTools: isBrowser,
+    ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
+    link: ApolloLink.from([errorLink, restLink, persistedQueryLink, httpLink]),
+    cache: new InMemoryCache().restore(initialState || {}),
+  })
+}
+
+export default function initApollo({
+  initialState = {},
+  headers,
+}: {
+  initialState: any
+  headers?: {[key: string]: string}
+}) {
+  // Make sure to create a new client for every server-side request so that data
+  // isn't shared between connections (which would be bad)
+  if (!isBrowser) {
+    return create(initialState, headers)
+  }
+
+  // Reuse client on the client-side
+  if (!apolloClient) {
+    apolloClient = create(initialState, headers)
+  }
+
+  return apolloClient
+}
--- index.js
+++ index.js
@@ -1,17 +1,66 @@
-// Don't change this code! ! ! ?
-const express = require('express')
+import {ApolloClient, InMemoryCache, HttpLink, ApolloLink} from 'apollo-boost'
+import {RestLink} from 'apollo-link-rest'
+import {createPersistedQueryLink} from 'apollo-link-persisted-queries'
+import fetch from 'isomorphic-unfetch'
+import getCsrfToken from '../util/getCsrfToken'
+import {onError} from 'apollo-link-error'
+import errorHandlerRegistry from './apolloErrorHandlerRegistry'
 
-const port = process.env.PORT || 3000
-const app = express()
+const isBrowser = process.browser
 
-app.get('/', (req, res) => {
-    res.send('<h1><marquee direction=right>💩</marquee></h1>')
-    res.end()
-})
+// Polyfill fetch() on the server (used by apollo-client)
+if (!isBrowser) {
+  ;(global as any).fetch = fetch
+  ;(global as any).Headers = require('node-fetch').Headers
+}
 
-app.listen(port, err => {
-    if (err) throw err
-    console.log(`>> Ready On Server http://localhost:${port}`)
-})
+let apolloClient: ApolloClient<{}> | null = null
 
-// NO
+const persistedQueryLink = createPersistedQueryLink()
+
+function create(initialState: any, headers: {[key: string]: string} = {}) {
+  if (isBrowser) headers['CSRF-Token'] = getCsrfToken()
+
+  const hostUrl = isBrowser ? location.origin : process.env.HOST_URL
+  const httpLink = new HttpLink({
+    uri: `${hostUrl}/graphql`, // Server URL (must be absolute)
+    credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+    headers,
+  })
+  const restLink = new RestLink({
+    uri: hostUrl,
+    credentials: 'same-origin',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json',
+    },
+  })
+  const errorLink = onError(e => errorHandlerRegistry.dispatch(e))
+  return new ApolloClient({
+    connectToDevTools: isBrowser,
+    ssrMode: !isBrowser, // Disables forceFetch on the server (so queries are only run once)
+    link: ApolloLink.from([errorLink, restLink, persistedQueryLink, httpLink]),
+    cache: new InMemoryCache().restore(initialState || {}),
+  })
+}
+
+export default function initApollo({
+  initialState = {},
+  headers,
+}: {
+  initialState: any
+  headers?: {[key: string]: string}
+}) {
+  // Make sure to create a new client for every server-side request so that data
+  // isn't shared between connections (which would be bad)
+  if (!isBrowser) {
+    return create(initialState, headers)
+  }
+
+  // Reuse client on the client-side
+  if (!apolloClient) {
+    apolloClient = create(initialState, headers)
+  }
+
+  return apolloClient
+}
